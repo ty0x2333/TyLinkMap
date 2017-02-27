cbb68525f44024f20a9ab9149a8e966e0741ba73
luckytianyiyan <luckytianyiyan@gmail.com>
2017-02-27 20:36:32 +0800

    M linkmap

--- tylinkmap/linkmap.py
+++ tylinkmap/linkmap.py
@@ -1,16 +1,73 @@
+import re
+import os
 PREFIX_PATH = '# Path: '
 PREFIX_ARCH = '# Arch: '
+PREFIX_OBJECT_FILES = '# Object files:'
+
+
+class FileObject(object):
+    def __init__(self, module, number=0, filename=''):
+        self.number = number
+        self.filename = filename
+        self.module = module
+
+    def __str__(self):
+        return "[%d] %s %s" % (self.number, self.filename, self.module)
 
 
 class LinkMap(object):
     def __init__(self):
         self.path = ''
         self.arch = ''
+        self.last_block = ''
+        self.file_objs = []
+
+    @property
+    def target_name(self):
+        return os.path.basename(self.path)
 
     def paring(self, filename):
         with open(filename, 'r') as f:
-            line = f.readline()
-            if line.startswith(PREFIX_PATH):
-                self.path = line[len(PREFIX_PATH):]
-            elif line.startswith(PREFIX_ARCH):
-                self.arch = line[len(PREFIX_ARCH):]
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+
+                if self.__paring_block(line):
+                    continue
+
+                if self.last_block == PREFIX_OBJECT_FILES:
+                    compiler = re.compile(r"\[\s*(?P<num>\d*)\]\s*(?P<content>.*)")
+                    content_compiler = re.compile(r"(?P<path>.*)\((?P<filename>.*)\)")
+                    match = compiler.match(line)
+                    if not match:
+                        # print 'Warning: can not match object file!'
+                        continue
+
+                    num = int(match.group('num'))
+                    content = match.group('content')
+                    # filename
+                    content_match = content_compiler.match(content)
+                    module = self.target_name
+                    if content_match:
+                        filename = content_match.group('filename')
+                        path = content_match.group('path')
+                        module = os.path.basename(path)
+                    else:
+                        filename = os.path.basename(content)
+
+                    self.file_objs.append(FileObject(module=module, number=num, filename=filename))
+                    print FileObject(module=module, number=num, filename=filename)
+
+    def __paring_block(self, line):
+        if line.startswith(PREFIX_PATH):
+            self.path = line[len(PREFIX_PATH):].strip()
+            self.last_block = PREFIX_PATH
+        elif line.startswith(PREFIX_ARCH):
+            self.arch = line[len(PREFIX_ARCH):]
+            self.last_block = PREFIX_ARCH
+        elif line.startswith(PREFIX_OBJECT_FILES):
+            self.last_block = PREFIX_OBJECT_FILES
+        else:
+            return False
+        return True
